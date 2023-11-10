c7a6746b54232186c80021814483b468c1ff7b3e
hongsh <hongsh@the-51.com>
2023-11-10 10:52:54 +0900

    vuetify 추가

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import App from './App.vue'
 import { router } from './router/index.js'
 import axios from 'axios'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/scss/index.scss'
 
 const app = createApp(App)
 app.provide('$axios', axios)
-createApp(App).use(ElementPlus).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(vuetify).use(router).mount('#app')
