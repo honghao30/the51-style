64b80d0cdd4ffc5076f48ce74b39fbc73e5c3858
hongsh <hongsh@the-51.com>
2023-11-10 11:48:53 +0900

    gnb 배열

---
+++ src/layouts/components/gnb.js
@@ -0,0 +1,93 @@
+const gnbList = [
+  {
+    'name': '영화정보',
+    'to': '',
+    'children': [
+        {
+          'name': '상영중 영화',
+          'to': '/MovieList',
+        },
+        {
+          'name': '박스오피스',
+          'to': '/BoxOffice',
+        }        
+      ]
+  },
+  {
+    'name': '컴포넌트',
+    'to': '',
+    'children': [
+        {
+          'name': '버튼',
+          'to': '/cmpButton',
+        },
+        {
+          'name': '타이틀',
+          'to': '/cmpTitle',
+        },
+        {
+          'name': '모달',
+          'to': '/cmpModal',
+        },
+        {
+          'name': '툴팁',
+          'to': '/cmpToolTip',
+        },
+        {
+          'name': '아코디언',
+          'to': '/cmpAccordion',
+        },
+        {
+          'name': '탭메뉴',
+          'to': '/cmpTab',
+        },
+        {
+          'name': 'SVG맵',
+          'to': '/cmpKoreaMap',
+        }            
+      ]
+  },
+  {
+    'name': '이벤트',
+    'to': '',
+    'children': [
+        {
+          'name': 'swiper',
+          'to': '',
+        },
+        {
+          'name': '스크롤이벤트',
+          'to': '/scrollEvent',
+        },
+        {
+          'name': '동영상 제어',
+          'to': '',
+        },
+        {
+          'name': '전국날씨',
+          'to': '/cmpKoreaWeather',
+        }           
+      ]
+  },
+  {
+    'name': '가이드',
+    'to': '',
+    'children': [
+        {
+          'name': '개요',
+          'to': '',
+        },
+        { 
+          'name': '설계 정책',
+          'to': '',
+        },
+        {
+          'name': '퍼블 리스트 샘플',
+          'to': '',
+        }          
+      ]
+  }     
+
+]
+
+export default gnbList
--- src/layouts/components/header.vue
+++ src/layouts/components/header.vue
@@ -11,60 +11,18 @@
         :ellipsis="false"
         @select="handleSelect"
       >        
-        <el-sub-menu index="1">
-          <template #title>영화정보</template>
-          <el-menu-item index="1-1">
-            <router-link to="/MovieList">인기영화</router-link>
+        <el-sub-menu
+          v-for="(gnb, index) in gnbList" :key="index"  
+          :index="1 + index"        
+        >
+          <template #title>{{ gnb.name }}</template>
+          <el-menu-item 
+            v-for="(subGnb, idx) in gnb.children" :key="idx"
+            :index="`${index} - 1 + ${idx}`"          
+          >
+            <router-link :to="subGnb.to">{{ subGnb.name }}</router-link>
           </el-menu-item>
-          <!-- <el-menu-item index="1-2">상영중</el-menu-item>           -->
-          <el-menu-item index="1-2">
-            <router-link to="/BoxOffice">박스오피스</router-link>
-          </el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="2">
-          <template #title>컴포넌트</template>
-          <el-menu-item index="2-1">
-            <router-link to="/cmpButton">버튼</router-link>
-          </el-menu-item>
-          <el-menu-item index="2-2">
-            <router-link to="/cmpTitle">타이틀</router-link>
-          </el-menu-item>
-          <el-menu-item index="2-3">
-            <router-link to="/cmpModal">모달</router-link>
-          </el-menu-item>
-          <el-menu-item index="2-4">
-            <router-link to="/cmpToolTip">말풍선</router-link>
-          </el-menu-item>
-          <el-menu-item index="2-5">
-            <router-link to="/cmpAccordion">아코디언</router-link>
-          </el-menu-item>   
-          <el-menu-item index="2-6">
-            <router-link to="/cmpTab">탭메뉴</router-link>
-          </el-menu-item>          
-          <el-menu-item index="2-7">
-            <router-link to="/cmpKoreaMap">SVG맵</router-link>
-          </el-menu-item>                 
-        </el-sub-menu>  
-        <el-sub-menu index="3">
-          <template #title>스크립트</template>
-          <el-menu-item index="3-1">swiper</el-menu-item>
-          <el-menu-item index="3-2">
-            <router-link to="/scrollEvent">스크롤이벤트</router-link>
-          </el-menu-item>
-          <el-menu-item index="3-3">동영상 제어</el-menu-item>
-          <el-menu-item index="3-4">
-            <router-link to="/cmpKoreaWeather">전국날씨</router-link>
-          </el-menu-item>          
-        </el-sub-menu> 
-        <el-sub-menu index="4">
-          <template #title>미니게임</template>
-          <el-menu-item index="4-1">롤렛</el-menu-item>
-          <el-menu-item index="4-1">주차관리</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="5">
-          <template #title>자료실</template>
-          <el-menu-item index="5-1">퍼블리싱 가이드</el-menu-item>          
-        </el-sub-menu>                     
+        </el-sub-menu>                   
       </el-menu>
     </div>
   </div>
@@ -72,6 +30,8 @@
 
 <script lang="ts" setup>
   import { ref } from 'vue'
+  import gnbList from './gnb'
+  
 
   const activeIndex = ref('1')
   const handleSelect = (key: string, keyPath: string[]) => {
